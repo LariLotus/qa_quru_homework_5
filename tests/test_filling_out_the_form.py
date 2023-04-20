1d46d0d7ed61a567db1ed0fe9093ff2ca5d04cca
Larisa Badmaeva <larilotus12@gmail.com>
2023-04-20 14:32:43 +0300

    Created a test to fill out the form

--- tests/test_filling_out_the_form.py
+++ tests/test_filling_out_the_form.py
@@ -9,7 +9,7 @@ def test_filling_out_the_form():
     browser.element('#lastName').type('Badmaeva')
     browser.element('#userEmail').type('larilotus12@gmail.com')
     browser.element('[value="Female"]').double_click()
-    browser.element('#userNumber').type('89022088667')
+    browser.element('#userNumber').type('8902208866')
 
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
@@ -18,7 +18,7 @@ def test_filling_out_the_form():
     browser.element('.react-datepicker__year-select').element('[value="1991"]').click()
     browser.element('.react-datepicker__day--012').click()
 
-    browser.element('#subjectsInput').type('Math').press_enter()
+    browser.element('#subjectsInput').type('Maths').press_enter()
 
     browser.element('[for="hobbies-checkbox-1"]').click()
 
@@ -30,8 +30,9 @@ def test_filling_out_the_form():
 
     browser.element('#submit').press_enter()
 
-    browser.all('#userForm').element_by(have.exact_texts(
-        'Student name Larisa Badmaeva', 'Student Email larilotus12@gmail.com', 'Gender Female',
-        'Mobile 89022088667', 'Date of Birth 12 July 1991', 'Subjects Math', 'Hobbies Sports',
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.all('tbody tr').should(have.exact_texts(
+        'Student Name Larisa Badmaeva', 'Student Email larilotus12@gmail.com', 'Gender Female',
+        'Mobile 89022088667', 'Date of Birth 12 July 1991', 'Subjects Maths', 'Hobbies Sports',
         'Picture IMG_4499.jpg', 'Current Address Moscow', 'State and City NCR Delhi'
     ))
