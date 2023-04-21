ad18f320b3af5d7773f71eaf2450d9ac939b485f
Larisa Badmaeva <larilotus12@gmail.com>
2023-04-21 15:39:30 +0300

    Created a test to fill out the form

--- conftest.py
+++ conftest.py
@@ -1,14 +1,12 @@
 import pytest
 from selene import browser
-from selenium import webdriver
+
 
 
 @pytest.fixture()
 def browser_managment():
-    # chrome_options = webdriver.ChromeOptions()
-    # browser.config.driver_options = chrome_options
     browser.config.base_url = 'https://demoqa.com'
-    # browser.driver.maximize_window()
+
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
