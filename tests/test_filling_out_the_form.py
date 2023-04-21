8273fb5947a6b7ebfe4000ac1629efe2352a3cff
Larisa Badmaeva <larilotus12@gmail.com>
2023-04-21 23:53:45 +0300

    Created a test to fill out the form

--- tests/test_filling_out_the_form.py
+++ tests/test_filling_out_the_form.py
@@ -3,7 +3,7 @@ import os
 
 
 def test_filling_out_the_form():
-    browser.open_url('/automation-practice-form')
+    browser.open('/automation-practice-form')
 
     browser.element('#firstName').type('Larisa')
     browser.element('#lastName').type('Badmaeva')
